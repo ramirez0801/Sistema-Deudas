d14be2a021a8b468d6636ebc96e39ad733af9346
ramirez0801 <ramirez.erg@hotmail.com>
2026-03-22 19:38:51 -0600

    First upload Created DBContext, Account and User Entities, TypeAccount Enum and Account Create and detail DTO

---
+++ SistemaDeudas/DTO/AccountDetailDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaDeudas.DTO
+{
+    public class AccountDetailDto
+    {
+        public int IdAccount { get; set; }
+        public string Name { get; set; }
+        public int Type { get; set; }
+        public string UserName{ get; set; } //Se puede cambiar el nombre del usario? Como se cual es el usuario? Esta propiedad es indepediente a User?
+    }
+}
